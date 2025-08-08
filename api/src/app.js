a5312f92e78e8b906ed36d14f64b0cdd12b80c1f
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-08 15:25:56 -0500

    feat: Agregar limitador de solicitudes a la ruta de músculos en app.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,19 @@
-import express from 'express';
-import musclesController from './controllers/musclesController.js';
+import express from "express";
+import rateLimit from "express-rate-limit";
+import musclesController from "./controllers/musclesController.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/v1/muscles', musclesController);
+const muscleLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minuto
+  max: 30, // Máximo 30 solicitudes por IP cada minuto
+  message: { error: "Too many requests, slow down!" },
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+});
+
+app.get("/api/v1/muscles", muscleLimiter, musclesController);
 
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
